e82f74a0c8b76c5b343ee004fb17e05676399483
Guilherme Miranda da Silva <guilherme.mirand@gmail.com>
2019-03-31 17:31:15 -0300

    Metodos e validações para salvar Hospital

--- src/test/java/gestao/apiTest/HospitalControllerTest.java
+++ src/test/java/gestao/apiTest/HospitalControllerTest.java
@@ -1,17 +1,26 @@
 package gestao.apiTest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gestao.model.Hospital;
 import gestao.serviceTest.HospitalService;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,10 +34,32 @@ public class HospitalControllerTest {
     @MockBean
     private HospitalService service;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     public void contextLoads() {
         assertThat(service).isNotNull();
         assertThat(mockMvc).isNotNull();
+    }
+
+    @Test
+    public void shouldSaveHospital() throws Exception {
+        Hospital hospital = this.buildValidHospital();
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
+                Hospital hospital = (Hospital) invocationOnMock.getArguments()[0];
+                hospital.setId(1L);
+                return hospital;
+            }
+        }).when(this.service).save(hospital);
+
+        this.mockMvc.perform(post("/hospitais")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.objectMapper.writeValueAsString(hospital)))
+                .andDo(print())
+                .andExpect(status().isCreated());
     }
 
     @Test
@@ -45,5 +76,14 @@ public class HospitalControllerTest {
         this.mockMvc.perform(get("/hospitais/30000"))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    private Hospital buildValidHospital() {
+        Hospital hospital = new Hospital();
+        hospital.setNome("Hospital de Teste");
+        hospital.setEndereco("Av. Brasil, 902");
+        hospital.setLatitude(new BigDecimal("10.058"));
+        hospital.setLongitude(new BigDecimal("-52.12"));
+        return hospital;
     }
 }
--- src/test/java/gestao/serviceTest/HospitalServiceTest.java
+++ src/test/java/gestao/serviceTest/HospitalServiceTest.java
@@ -3,21 +3,31 @@ package gestao.serviceTest;
 import gestao.model.Hospital;
 import gestao.repository.HospitalRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
 
 @RunWith(SpringRunner.class)
 public class HospitalServiceTest {
+
+    private Validator validator;
 
     @Autowired
     private HospitalService service;
@@ -34,14 +44,82 @@ public class HospitalServiceTest {
         }
     }
 
+    @Before
+    public void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        this.validator = factory.getValidator();
+    }
+
     @Test
     public void contextLoads() {
         Assertions.assertThat(service).isNotNull();
     }
 
     @Test
+    public void shouldSaveHospital() {
+        Hospital hospital = this.buildValidHospital();
+        Hospital saved = this.trySaveHospital(hospital);
+        assertThat(saved.getId(), is(notNullValue()));
+    }
+
+    @Test
+    public void shouldNotSaveHospitalWithoutName() {
+        Hospital hospital = this.buildValidHospital();
+        hospital.setNome(null);
+        Hospital saved = this.trySaveHospital(hospital);
+        assertThat(saved, is(nullValue()));
+    }
+
+    @Test
+    public void shouldNotSaveHospitalWithoutEndereco() {
+        Hospital hospital = this.buildValidHospital();
+        hospital.setEndereco(null);
+        Hospital saved = this.trySaveHospital(hospital);
+        assertThat(saved, is(nullValue()));
+    }
+
+    @Test
+    public void shouldNotSaveHospitalWithInvalidLatitude() {
+        Hospital hospital = this.buildValidHospital();
+        hospital.setLatitude(new BigDecimal("90.1"));
+        Hospital saved = this.trySaveHospital(hospital);
+        assertThat(saved, is(nullValue()));
+    }
+
+    @Test
+    public void shouldNotSaveHospitalWithInvalidLongitude() {
+        Hospital hospital = this.buildValidHospital();
+        hospital.setLongitude(new BigDecimal("-190"));
+        Hospital saved = this.trySaveHospital(hospital);
+        assertThat(saved, is(nullValue()));
+    }
+
+    private Hospital trySaveHospital(Hospital hospital) {
+        Mockito.when(this.repository.save(hospital)).thenReturn(this.saveHospital(hospital));
+        return this.service.save(hospital);
+    }
+
+    private Hospital saveHospital(Hospital hospital) {
+        Set<ConstraintViolation<Hospital>> violations = this.validator.validate(hospital);
+        if (violations.isEmpty()) {
+            hospital.setId(1L);
+            return hospital;
+        }
+        return null;
+    }
+
+    @Test
     public void shouldReturnAllHospital() {
         List<Hospital> hospitals = service.listAll();
         assertThat(hospitals, hasSize(0));
+    }
+
+    private Hospital buildValidHospital() {
+        Hospital hospital = new Hospital();
+        hospital.setNome("Hospital de Teste");
+        hospital.setEndereco("Av. Brasil, 902");
+        hospital.setLatitude(new BigDecimal("10.058"));
+        hospital.setLongitude(new BigDecimal("-52.12"));
+        return hospital;
     }
 }
