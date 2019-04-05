8a8abd34a5bfe31ae6fb0508bd447d99b1acf565
Guilherme Miranda da Silva <guilherme.mirand@gmail.com>
2019-04-05 20:35:08 -0300

    Operações básicas com paciente

--- src/main/java/gestao/api/PacienteController.java
+++ src/main/java/gestao/api/PacienteController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
@@ -17,24 +19,32 @@ public class PacienteController {
     @Autowired
     private PacienteService service;
 
+    @ResponseBody
     @PostMapping
-    // TODO Implementar método para salvar paciente
-    public ResponseEntity<?> save(@RequestBody Paciente paciente) throws URISyntaxException {
-        return null;
+    public ResponseEntity<?> save(@Valid @RequestBody Paciente paciente) throws URISyntaxException {
+        return ResponseEntity.created(new URI(this.service.save(paciente).getId().toString())).build();
     }
 
+    @ResponseBody
     @GetMapping
-    // TODO Implementar método para retornar a lista de pacientes
     public ResponseEntity<List<Paciente>> listAll(){
-        return null;
+        return ResponseEntity.ok(this.service.listAll());
     }
 
+    @ResponseBody
     @GetMapping("/{id}")
-    // TODO Implementar método para buscar por id, mostrar detalhes
     public ResponseEntity<Paciente> findById(@PathVariable("id") Long id){
-        return null;
+        return ResponseEntity.ok(this.service.findById(id));
     }
 
+    @ResponseBody
+    @PutMapping("/{id}")
+    public ResponseEntity<Paciente> update(@PathVariable("id") Long id, @Valid @RequestBody Paciente paciente) {
+        this.service.update(id, paciente);
+        return ResponseEntity.ok().build();
+    }
+
+    @ResponseBody
     @GetMapping("/{id}/hospitais-proximos")
     // TODO Listar todos os hospitais próximos do paciente
     public ResponseEntity<List<Hospital>> listAllNearby(@PathVariable("id") Long id) {
