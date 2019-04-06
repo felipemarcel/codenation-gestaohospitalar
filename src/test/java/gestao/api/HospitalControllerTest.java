88c37a82cce112023900d3efe6fbfc38aef769a2
Guilherme Miranda da Silva <guilherme.mirand@gmail.com>
2019-04-06 16:07:55 -0300

    checkin e checkout de paciente

--- src/test/java/gestao/api/HospitalControllerTest.java
+++ src/test/java/gestao/api/HospitalControllerTest.java
@@ -1,9 +1,16 @@
 package gestao.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import gestao.exception.CheckinNotValidException;
+import gestao.exception.CheckoutNotValidException;
 import gestao.exception.HospitalNotFoundException;
 import gestao.model.Hospital;
+import gestao.model.Internacao;
+import gestao.model.Paciente;
+import gestao.model.Sexo;
 import gestao.service.HospitalService;
+import gestao.service.InternacaoService;
+import gestao.service.PacienteService;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -24,10 +31,9 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.allOf;
@@ -46,12 +52,22 @@ public class HospitalControllerTest {
     @MockBean
     private HospitalService service;
 
+    @MockBean
+    private PacienteService pacienteService;
+
+    @MockBean
+    private InternacaoService internacaoService;
+
     @Autowired
     private ObjectMapper objectMapper;
 
     private Validator validator;
 
     private Map<Long, Hospital> fakeRepository = new HashMap<>();
+
+    private Map<Long, Internacao> fakeInternacaoRepository = new HashMap<>();
+
+    private Long idInternacaoCount = 0L;
 
     private Long idCount = 0L;
 
@@ -71,6 +87,50 @@ public class HospitalControllerTest {
                 return hospital;
             }
             return null;
+        });
+    }
+
+    private Stubber getDoAnswerToInternacaoSave() {
+        return Mockito.doAnswer(invocation -> {
+            Internacao internacao = (Internacao) invocation.getArguments()[0];
+            if (getInternacaoAberta(internacao.getPaciente().getId()) == null) {
+                Set<ConstraintViolation<Internacao>> violations = validator.validate(internacao);
+                if (violations.isEmpty()) {
+                    internacao.setId(++idInternacaoCount);
+                    fakeInternacaoRepository.put(internacao.getId(), internacao);
+                    return internacao;
+                }
+            } else {
+                throw new CheckinNotValidException();
+            }
+            return null;
+        });
+    }
+
+    private Internacao getInternacaoAberta(Long idPaciente) {
+        Optional<Internacao> optional = this.fakeInternacaoRepository.values().stream()
+                .filter(item -> (item.getPaciente().getId().equals(idPaciente)
+                        && item.getDataSaida() == null))
+                .findAny();
+        if (!optional.isPresent()) {
+            return null;
+        }
+        return optional.get();
+    }
+
+    private Stubber getDoAnswerToInternacaoCheckout() {
+        return Mockito.doAnswer(invocation -> {
+            Long idPaciente = (Long) invocation.getArguments()[0];
+            Long idHospital = (Long) invocation.getArguments()[1];
+            Internacao internacao = getInternacaoAberta(idPaciente);
+
+            if (internacao == null || !internacao.getHospital().getId().equals(idHospital)) {
+                throw new CheckoutNotValidException("O paciente não está internado neste hospital.");
+            }
+
+            internacao.setDataSaida(LocalDateTime.now());
+            fakeInternacaoRepository.put(internacao.getId(), internacao);
+            return internacao;
         });
     }
 
@@ -100,6 +160,15 @@ public class HospitalControllerTest {
         });
     }
 
+    private Stubber getDoAnswerToPacienteFindById() {
+        // Será considerado que existe o paciente
+        return Mockito.doAnswer(invocation -> {
+            Paciente paciente = this.buildValidPaciente();
+            paciente.setId((Long) invocation.getArguments()[0]);
+            return paciente;
+        });
+    }
+
     private Stubber getDoAnswerToList() {
         return Mockito.doAnswer(invocation -> fakeRepository.values().stream().collect(Collectors.toList()));
     }
@@ -112,6 +181,9 @@ public class HospitalControllerTest {
         this.getDoAnswerToList().when(this.service).listAll();
         this.getDoAnswerToUpdate().when(this.service).update(Mockito.anyLong(), Mockito.any(Hospital.class));
         this.getDoAnswerToFindById().when(this.service).findBy(Mockito.anyLong());
+        this.getDoAnswerToPacienteFindById().when(this.pacienteService).findById(Mockito.anyLong());
+        this.getDoAnswerToInternacaoSave().when(this.internacaoService).save(Mockito.any(Internacao.class));
+        this.getDoAnswerToInternacaoCheckout().when(this.internacaoService).checkout(Mockito.anyLong(), Mockito.anyLong());
     }
 
     @Test
@@ -160,6 +232,57 @@ public class HospitalControllerTest {
                 .andExpect(status().is4xxClientError());
     }
 
+    @Test
+    public void shouldCheckinPaciente() throws Exception {
+        Hospital hospital = this.buildValidHospital();
+        service.save(hospital);
+
+        this.mockMvc.perform(post("/hospitais/1/pacientes/1/checkin"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    private void checkinPacienteId1() {
+        Hospital hospital = this.buildValidHospital();
+        Paciente paciente = this.buildValidPaciente();
+        Internacao internacao = new Internacao();
+        hospital = service.save(hospital);
+        paciente.setId(1L);
+        internacao.setPaciente(paciente);
+        internacao.setHospital(hospital);
+        internacao.setDataEntrada(LocalDateTime.now());
+        internacao.setId(1L);
+        this.fakeInternacaoRepository.put(1L, internacao);
+    }
+
+    @Test
+    public void shouldNotCheckinPacienteInternado() throws Exception {
+        this.checkinPacienteId1();
+        this.mockMvc.perform(post("/hospitais/1/pacientes/1/checkin"))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void shouldCheckoutPacienteInternado() throws Exception {
+        this.checkinPacienteId1();
+        this.mockMvc.perform(put("/hospitais/1/pacientes/1/checkout"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldNotCheckoutPacienteNaoInternado() throws Exception {
+        this.checkinPacienteId1();
+        this.mockMvc.perform(put("/hospitais/1/pacientes/1/checkout"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        this.mockMvc.perform(put("/hospitais/1/pacientes/1/checkout"))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+
     private Hospital buildValidHospital() {
         Hospital hospital = new Hospital();
         hospital.setNome("Hospital de Teste");
@@ -167,5 +290,17 @@ public class HospitalControllerTest {
         hospital.setLatitude(new BigDecimal("10.058"));
         hospital.setLongitude(new BigDecimal("-52.12"));
         return hospital;
+    }
+
+    private Paciente buildValidPaciente() {
+        Paciente paciente = new Paciente();
+        paciente.setNomeCompleto("Maria da Silva");
+        paciente.setCpf("371.310.164-70");
+        paciente.setSexo(Sexo.F);
+        paciente.setDataNascimento(LocalDate.of(1992, 10, 20));
+        paciente.setEndereco("R. Gen. Lima e Silva, 606 - Cidade Baixa - Porto Alegre - RS, 90050-102");
+        paciente.setLatitude(new BigDecimal("-30.038260"));
+        paciente.setLongitude(new BigDecimal(-51.221581));
+        return paciente;
     }
 }
