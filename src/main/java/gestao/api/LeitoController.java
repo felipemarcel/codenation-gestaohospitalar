82a52cf9de0972184942d5f6b275b56fd62493ca
Luiz <fsa16282@outlook.com>
2019-04-07 10:51:24 -0300

    Merge branch 'developer' of https://github.com/felipemarcel/codenation-gestaohospitalar into produto
    # Conflicts:
    #	src/main/java/gestao/api/HospitalController.java
    #	src/main/java/gestao/exception/NotFoundAdvice.java
    #	src/main/resources/ValidationMessages.properties
    #	src/test/java/gestao/api/HospitalControllerTest.java

---
+++ src/main/java/gestao/api/LeitoController.java
@@ -0,0 +1,15 @@
+package gestao.api;
+
+import gestao.service.LeitoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/leito")
+public class LeitoController {
+
+    @Autowired
+    private LeitoService service;
+
+}
--- src/main/java/gestao/service/LeitoService.java
+++ src/main/java/gestao/service/LeitoService.java
@@ -1,4 +1,12 @@
 package gestao.service;
 
+import gestao.repository.LeitoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class LeitoService {
+
+    @Autowired
+    private LeitoRepository repository;
+
+
 }
