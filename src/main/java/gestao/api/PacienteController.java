0149922ce8e087215713fef0827ce7761297c1e3
Felipe Marcel <felipemarcelqs@gmail.com>
2019-04-07 20:41:24 -0300

    Adicionada api do google maps

--- src/main/java/gestao/api/PacienteController.java
+++ src/main/java/gestao/api/PacienteController.java
@@ -74,6 +74,7 @@ public class PacienteController {
     @GetMapping("/{id}/hospitais-proximos")
     // TODO Listar todos os hospitais próximos do paciente
     public ResponseEntity<List<Hospital>> listAllNearby(@PathVariable("id") Long id) {
+
         return null;
     }
 }
